8d7be49a2c8bf4c62a671efd556ea09277c25ff7
KevinWesseler <xkivx@web.de>
2016-02-09 16:47:48 +0100

    #USID000
    #Datum 05.02.2016
    #Zeit 1h
    #Info
    - Tagesänderungen

---
+++ Workspace/Scrumiverse/src/com/scrumiverse/web/TestController.java
@@ -0,0 +1,19 @@
+package com.scrumiverse.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.scrumiverse.model.account.User;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/machmal.htm")
+	public ModelAndView test(){
+		ModelMap map=new ModelMap();
+		map.addAttribute("dummy", new User());
+		return new ModelAndView("test", map);
+	}
+}
