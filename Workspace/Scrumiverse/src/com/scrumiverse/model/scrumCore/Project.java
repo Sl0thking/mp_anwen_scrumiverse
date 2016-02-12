d381b913836de8daddc2d65b3a3807ce1d830479
DoctorWhose <toniserfling@aol.com>
2016-02-12 15:28:21 +0100

    #USID011
    - Auskommentierung von nicht funktionalem Code

--- Workspace/Scrumiverse/src/com/scrumiverse/model/scrumCore/Project.java
+++ Workspace/Scrumiverse/src/com/scrumiverse/model/scrumCore/Project.java
@@ -18,11 +18,11 @@ public class Project {
 	private int projectID;
 	private String name;
 	private String description;
-	private List<Role> roles;
-	private Map<Role, User> users;
-	private List<Sprint> sprints;
-	private List<UserStory> userstories;
-	private List<Category> categories;
+	//private List<Role> roles;
+	//private Map<Role, User> users;
+	//private List<Sprint> sprints;
+	//private List<UserStory> userstories;
+	//private List<Category> categories;
 	
 		
 	@Id
@@ -45,81 +45,81 @@ public class Project {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public List<Role> getRoles() {
-		return roles;
-	}
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
-	public Map<Role, User> getUsers() {
-		return users;
-	}
-	public void setUsers(Map<Role, User> users) {
-		this.users = users;
-	}
-	public List<Sprint> getSprints() {
-		return sprints;
-	}
-	public void setSprints(List<Sprint> sprints) {
-		this.sprints = sprints;
-	}
-	public List<UserStory> getUserstories() {
-		return userstories;
-	}
-	public void setUserstories(List<UserStory> userstories) {
-		this.userstories = userstories;
-	}
-	public List<Category> getCategories() {
-		return categories;
-	}
-	public void setCategories(List<Category> categories) {
-		this.categories = categories;
-	}
+//	public List<Role> getRoles() {
+//		return roles;
+//	}
+//	public void setRoles(List<Role> roles) {
+//		this.roles = roles;
+//	}
+//	public Map<Role, User> getUsers() {
+//		return users;
+//	}
+//	public void setUsers(Map<Role, User> users) {
+//		this.users = users;
+//	}
+//	public List<Sprint> getSprints() {
+//		return sprints;
+//	}
+//	public void setSprints(List<Sprint> sprints) {
+//		this.sprints = sprints;
+//	}
+//	public List<UserStory> getUserstories() {
+//		return userstories;
+//	}
+//	public void setUserstories(List<UserStory> userstories) {
+//		this.userstories = userstories;
+//	}
+//	public List<Category> getCategories() {
+//		return categories;
+//	}
+//	public void setCategories(List<Category> categories) {
+//		this.categories = categories;
+//	}
 	
-	public void addCategory(Category c) {		
-		this.categories.add(c);				
-	}
-	
-	public void deleteCategory(int CategoryID) {
-		this.categories.remove(CategoryID);		
-	}
-	
-	public void addUser(User u) {		
-		this.users.put(null, u);		
-	}
-	
-	public void removeUser(int userID) {
-		this.users.remove(userID);		
-	}
-	
-	public void setRole(int userID, Role r) {
-		this.roles.set(userID, r);
-	}
-	
-	public void deleteRole(int RoleID) {
-		this.roles.remove(RoleID);
-	}
-	
-	public void addRole(Role r) {
-		this.roles.add(r);
-	}
-	
-	public void addSprint(Sprint s) {
-		this.sprints.add(s);
-	}
-	
-	public void removeSprint(int sprintID) {
-		this.sprints.remove(sprintID);
-	}
-	
-	public void addUserStory(UserStory u) {
-		this.userstories.add(u);
-	}
-	
-	public void removeUserStory(int UserStoryID) {
-		this.userstories.remove(UserStoryID);
-	}
-	
+//	public void addCategory(Category c) {		
+//		this.categories.add(c);				
+//	}
+//	
+//	public void deleteCategory(int CategoryID) {
+//		this.categories.remove(CategoryID);		
+//	}
+//	
+//	public void addUser(User u) {		
+//		this.users.put(null, u);		
+//	}
+//	
+//	public void removeUser(int userID) {
+//		this.users.remove(userID);		
+//	}
+//	
+//	public void setRole(int userID, Role r) {
+//		this.roles.set(userID, r);
+//	}
+//	
+//	public void deleteRole(int RoleID) {
+//		this.roles.remove(RoleID);
+//	}
+//	
+//	public void addRole(Role r) {
+//		this.roles.add(r);
+//	}
+//	
+//	public void addSprint(Sprint s) {
+//		this.sprints.add(s);
+//	}
+//	
+//	public void removeSprint(int sprintID) {
+//		this.sprints.remove(sprintID);
+//	}
+//	
+//	public void addUserStory(UserStory u) {
+//		this.userstories.add(u);
+//	}
+//	
+//	public void removeUserStory(int UserStoryID) {
+//		this.userstories.remove(UserStoryID);
+//	}
+//	
 	@Override
 	public String toString() {
 		return "Project [projectID=" + projectID + ", name=" + name + ", description=" + description + ", roles="
