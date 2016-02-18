b5510698fd176ccfe526a8747f56d2622eaef8a2
Sl0thking <k.jolitz-dev@outlook.de>
2016-02-18 17:30:14 +0100

    #USID018
    #Datum 18.02.2016
    #Zeit 1h
    #Info
    -Rechte und Rollen implementiert und hibernate fähig gemacht

--- Workspace/Scrumiverse/src/com/scrumiverse/model/account/Right.java
+++ Workspace/Scrumiverse/src/com/scrumiverse/model/account/Right.java
@@ -1,5 +1,29 @@
 package com.scrumiverse.model.account;
 
+/**
+ * Enum of different user rights
+ * 
+ * @author Kevin Jolitz
+ * @version 18.02.2016
+ *
+ */
 public enum Right {
-
+	Create_UserStory,
+	Delete_UserStory,
+	Edit_UserStory,
+	Read_UserStory,
+	Create_Task,
+	Delete_Task,
+	Edit_Task,
+	Read_Task,
+	Create_Sprint,
+	Delete_Sprint,
+	Edit_Sprint,
+	Read_Sprint,
+	Invite_To_Project,
+	Delete_From_Project,
+	View_Review,
+	AlertAllNotifications_From_CurrentSprint,
+	Define_Role,
+	Change_Role
 }
--- Workspace/Scrumiverse/src/com/scrumiverse/model/account/Role.java
+++ Workspace/Scrumiverse/src/com/scrumiverse/model/account/Role.java
@@ -1,13 +1,27 @@
 package com.scrumiverse.model.account;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.CollectionOfElements;
+
+/**
+ * Role of a scrumiverse user
+ * 
+ * @author Kevin Jolitz
+ * @version 18.02.2016
+ */
 @Entity
 public class Role {
 	
 	private int roleID;
+	private String name;
+	private List<Right> rights;
 
 	@Id
 	@GeneratedValue
@@ -18,7 +32,45 @@ public class Role {
 	public void setRoleID(int roleID) {
 		this.roleID = roleID;
 	}
-	
-	
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean hasRights(Right right) {
+		return rights.contains(right);
+	}
+	
+	public void addRight(Right right) {
+		rights.add(right);
+	}
+	
+	public void deleteRight(Right right) {
+		rights.remove(right);
+	}
+	
+	/**
+	 * Create a deep copy of this role
+	 * @return Copy of this role
+	 */
+	public Role copy() {
+		Role copyRole = new Role();
+		copyRole.setName(this.name);
+		copyRole.setRights(this.getRights());
+		return copyRole;
+	}
+	
+	@CollectionOfElements(targetElement = Right.class)
+	@Enumerated(EnumType.STRING)
+	public List<Right> getRights() {
+		return rights;
+	}
+
+	public void setRights(List<Right> rights) {
+		this.rights = rights;
+	}
 }
