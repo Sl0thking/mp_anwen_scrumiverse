06a1e8d52833ac01049f145bcdb09a5f68ed69bd
DoctorWhose <toniserfling@aol.com>
2016-02-24 10:46:57 +0100

    #USID013
    #Datum 24.02.2016
    #Zeit 20min
    #Info
    - Javadoc hinzugefügt
    - getUserStoriesByState und equals-Überschreibung implementiert

--- Workspace/Scrumiverse/src/com/scrumiverse/model/scrumCore/Sprint.java
+++ Workspace/Scrumiverse/src/com/scrumiverse/model/scrumCore/Sprint.java
@@ -12,7 +12,7 @@ import javax.persistence.Transient;
 
 /**
  * Datamodel of Sprints
- * @author DoctorWhose
+ * @author Toni Serfling
  * @version 23.02.2016
  */
 @Entity
@@ -61,6 +61,10 @@ public class Sprint extends PlanElement {
 		return userStories.get(usID);
 	}
 	
+	/**
+	 * Returns the number of finished User Stories 
+	 * @return int finishedUserStories
+	 */
 	@Transient
 	public int getFinishedUserStories() {
 		
@@ -73,11 +77,25 @@ public class Sprint extends PlanElement {
 		return finishedUserStories;	
 	}
 	
-	public UserStory getUserStoriesByState(PlanState plan) {
-			
-		return null;		
+	/**
+	 * Returns all User Stories of the given PlanState
+	 * @param plan
+	 * @return List<UserStory> userStoriesByState;
+	 */
+	public List<UserStory> getUserStoriesByState(PlanState plan) {
+		List<UserStory> userStoriesByState = new ArrayList<UserStory>();
+		for(UserStory us:userStories){
+			if(us.getPlanState()== plan) {
+				userStoriesByState.add(us);
+			}
+		}		
+		return userStoriesByState;		
 	}
 	
+	/**
+	 * Returns the planned time of the sprint in minutes
+	 * @return int plannedMinutes
+	 */
 	@Transient
 	public int getPlannedMinutes() {
 		int plannedMinutes = 0;		
@@ -87,6 +105,10 @@ public class Sprint extends PlanElement {
 		return plannedMinutes;
 	}
 	
+	/**
+	 * Returns the already spent time of the sprint in minutes
+	 * @return int workedMinutes
+	 */
 	@Transient
 	public int getWorkedMinutes() {
 		int workedMinutes = 0;
@@ -97,6 +119,10 @@ public class Sprint extends PlanElement {
 		return workedMinutes;
 	}
 	
+	/**
+	 * Returns the remaining time of the sprint in minutes
+	 * @return int remainingMinutes
+	 */
 	@Transient
 	public int getRemainingMinutes() {
 		int remainingMinutes = 0;
@@ -104,6 +130,20 @@ public class Sprint extends PlanElement {
 			remainingMinutes += us.getRemainingMinutes();
 		}
 		return remainingMinutes;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sprint other = (Sprint) obj;
+		if (getId() != other.getId())
+			return false;
+		return true;
 	}
 	
 }
